91b206602f60e3d698109daa4458b68aa666bebe
Jasmine Baik <boshnivolo@gmail.com>
2016-04-06 11:01:19 -0400

    Create scopes to compare models to API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var LoginTest = angular.module('LoginTest', []);
 
 LoginTest.controller('LoginController', ['$scope', '$http', function($scope, $http){
+
+  // Create a scope for a user
+  $scope.user = {
+    email = '',
+    password = ''
+  };
+
+  $scope.login = function(user){
+    // $http goes here in order to check against existing user credentials
+  }
+
   console.log("YO");
 }]);
